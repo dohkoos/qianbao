b964353c8511e9114829ef87e3ae4b21388891b1
Peter Wong <dohkoos@gmail.com>
2010-01-02 09:11:10 +0800

    Added i18n support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,18 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
 
+  before_filter :set_locale
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  protected
+    def set_locale
+      accept_lang = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      I18n.locale = params[:locale] || session[:locale] || accept_lang || 'zh'
+      session[:locale] = I18n.locale
+    end
 end
