2a99c180ab6ed7dfa42dab19414c2119958875ca
Peter Wong <dohkoos@gmail.com>
2010-01-07 18:19:18 +0800

    Replace tags field with acts_as_taggable_on_steroids

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ActiveRecord::Base
+  acts_as_taggable
+
   belongs_to :user
 end
---
+++ db/migrate/20100107181546_remove_tags_from_entries.rb
@@ -0,0 +1,9 @@
+class RemoveTagsFromEntries < ActiveRecord::Migration
+  def self.up
+    remove_column :entries, :tags
+  end
+
+  def self.down
+    add_column :entries, :tags, :string
+  end
+end
